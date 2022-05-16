a2e7139a6a3478fbcb9b2554204d24953141f9ab
davindersrai <daverai@yahoo.com>
2022-05-16 08:11:05 -0700

    Update cli_webApp.ps1

--- cli_webApp.ps1
+++ cli_webApp.ps1
@@ -16,3 +16,7 @@ az webapp list-runtimes --linux
 
 az webapp create  --name "webapps-dev-mp200202"  --plan "webapps-dev-plan" --resource-group "rgLearn4" --runtime "NODE:14-LTS"  
 
+#my deployment
+az appservice plan create --name "railoadtrac2" --resource-group "raient1" --sku "s1"
+az webapp create --name "railoadtrac2" --plan "railoadtrac2" --resource-group "raient1" --runtime "ASPNET:V4.8"
+ az webapp deployment source config --branch "main" --manual-integration --name "railoadtrac" --repo-url "https://github.com/DavinderLearning/ArTraxNoSource.git" --resource-group "raient1"
